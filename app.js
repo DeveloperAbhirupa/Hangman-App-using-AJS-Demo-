b549911376e966dcd3df54a4ae30a80dc36694fd
DeveloperAbhirupa <mitra.abhirupa@gmail.com>
2018-02-06 01:25:04 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var app=angular.module("HangmanApp",[]); //Name of the app, Dependencies
+app.controller("GameController",['$scope',function($scope){                      //Defining controller which will link .js with html
+$scope.demo= "somestring";                                                      //Pass a function which takes a parameter $scope;
+                                                                                // $scope will be availbale in html page, dependency injection
+
+
+
+
+
+}])
