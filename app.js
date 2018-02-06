85384bf24c4c826032311b5eceaad75df4607b42
DeveloperAbhirupa <mitra.abhirupa@gmail.com>
2018-02-07 00:27:10 +0530

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 var app=angular.module("HangmanApp",[]); //Name of the app, Dependencies
 app.controller("GameController",['$scope',function($scope){                      //Defining controller which will link .js with html
-$scope.demo= "somestring";                                                      //Pass a function which takes a parameter $scope;
+                                                                                //Pass a function which takes a parameter $scope;
                                                                                 // $scope will be availbale in html page, dependency injection
+var  words=['positivity', 'chaos', 'intellectual', 'paranormal','function','dimention','liberty']   ;
+$scope.incorectLetters=[];
+$scope.correctLetters=[];
+var guesses=5;
+$scope.guesses=5;
+$scope.display='';
+$scope.input={
+  letter:''
+}
+var selectRandomWord = function(){
+  var ind=Math.round(Math.random()*(words.length-1));
+  return words[ind];
+}
+var newGame = function(){
+  $scope.incorectLetters=[];
+  $scope.correctLetters=[];
+  var guesses=5;
+  $scope.guesses=5;
+  var word=selectRandomWord();
+  var temp=''
+  for(var i=1;i<=word.length;i++)
+  {  temp+='*';
+  }
+  $scope.display=temp;
+  console.log(temp);
+}
+
+newGame();
 
 
 
 
 
-}])
+
+}]);
